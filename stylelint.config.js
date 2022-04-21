325c22bb1312c7700ff6e8574ba83040392e9cb3
praburangki <praburangki@gmail.com>
2022-04-21 16:48:18 +0800

    feat: reenable prettier

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['stylelint-config-standard'],
+  extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
 
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
