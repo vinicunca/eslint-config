a4f726224d174d76c99d81c2d545843bf81dff4b
praburangki <praburangki@gmail.com>
2022-04-26 16:45:29 +0800

    feat: remove prettier

--- index.js
+++ index.js
@@ -4,12 +4,5 @@ module.exports = defineConfig({
   extends: [
     './react.eslint.js',
     './vue.eslint.js',
-
-    /**
-     * Make sure to put prettier last, so it gets the chance to override other configs.
-     *
-     * @see https://github.com/prettier/eslint-config-prettier
-     */
-    'plugin:prettier/recommended',
   ],
 });
