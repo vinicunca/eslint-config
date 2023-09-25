7e9b15ac5853a722dbea78077f889226641d65df
praburangki <praburangki@gmail.com>
2023-09-25 08:20:54 +0800

    fix: import vinicunca plugin typo

--- eslint/src/plugins.ts
+++ eslint/src/plugins.ts
@@ -3,6 +3,7 @@
 
 export { default as pluginStylisticJs } from '@stylistic/eslint-plugin-js';
 export { default as pluginStylisticTs } from '@stylistic/eslint-plugin-ts';
+export { default as pluginVinicunca } from '@vinicunca/eslint-plugin-vinicunca';
 export { default as pluginTs } from '@typescript-eslint/eslint-plugin';
 export { default as parserTs } from '@typescript-eslint/parser';
 export { default as pluginComments } from 'eslint-plugin-eslint-comments';
@@ -18,7 +19,6 @@ export { default as pluginUnicorn } from 'eslint-plugin-unicorn';
 export { default as pluginUnusedImports } from 'eslint-plugin-unused-imports';
 export { default as pluginReact } from 'eslint-plugin-react';
 export { default as pluginReactHooks } from 'eslint-plugin-react-hooks';
-export { default as pluginVinicunca } from 'eslint-plugin-vinicunca';
 export { default as pluginVue } from 'eslint-plugin-vue';
 export { default as pluginYml } from 'eslint-plugin-yml';
 export { default as parserJsonc } from 'jsonc-eslint-parser';
