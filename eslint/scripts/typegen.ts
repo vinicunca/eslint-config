7147550c443c6ea44dcc2cc625f92d75998d7960
praburangki <praburangki@gmail.com>
2024-03-31 09:23:41 +0800

    chore: update deps

--- eslint/scripts/typegen.ts
+++ eslint/scripts/typegen.ts
@@ -1,9 +1,17 @@
+import { builtinRules } from 'eslint/use-at-your-own-risk';
 import { flatConfigsToRulesDTS } from 'eslint-typegen/core';
 import fs from 'node:fs/promises';
 
 import { combineConfigs, comments, formatters, imports, javascript, jsdoc, jsonc, markdown, node, perfectionist, react, sortPackageJson, stylistic, test, typescript, unicorn, unocss, vue, yaml } from '../src';
 
 const configs = await combineConfigs(
+  {
+    plugins: {
+      '': {
+        rules: Object.fromEntries(builtinRules.entries()),
+      },
+    },
+  },
   comments(),
   formatters(),
   imports(),
