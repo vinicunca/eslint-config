bdfe30a1fe6b087cd72e345356504c0fd6458d27
praburangki <praburangki@gmail.com>
2026-05-12 08:17:25 +0800

    chore: update deps

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'tsdown';
+import { StaleGuardRecorder } from 'tsdown-stale-guard';
 
 export default defineConfig({
   entry: [
     'src/index.ts',
   ],
+  dts: true,
   shims: true,
   format: ['esm'],
   exports: true,
+  plugins: [
+    StaleGuardRecorder(),
+  ],
 });
